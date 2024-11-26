13207d0e774332d271df12724478b7b744e034a8
Micro-Organism <coolrabbit1993@gmail.com>
2024-11-26 20:15:36 +0800

    feat: springboot 集成 airflow

---
+++ zero-apache-airflow/src/test/java/com/zero/apache/airflow/ZeroApacheAirflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.apache.airflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroApacheAirflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
